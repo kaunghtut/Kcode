205795057e45768c688b85e8ee0e683ccdf0ff69
kaunghtut <kaunghtut.kh2041998@gmail.com>
2025-09-18 18:59:03 +0630

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ fetch('poems.json')
         poems.forEach(poem => {
             const block = document.createElement('div');
             block.className = 'preview-block';
-
+            const lines = poem.title;
             const lines = poem.content.split('\n');
             const previewContent = lines.slice(0, PREVIEW_LINES).join('\n');
             const isTruncated = lines.length > PREVIEW_LINES;
@@ -70,4 +70,5 @@ document.addEventListener('keydown', (e) => {
         document.body.style.overflow = '';
     }
 });
+
 
